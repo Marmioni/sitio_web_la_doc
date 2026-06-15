fbea1969654e993329fd4025958886d9fd56a773
Ronnie <ronnie@ghost>
2026-06-15 15:08:17 -0600

    Update contact page to use Instagram

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,8 @@
+export const socialLinks = [
+  { label: "Instagram", url: "https://www.instagram.com/mar.mioni?igsh=aDRjYndsYTY4bjIw" },
+  { label: "YouTube", url: "https://youtube.com/@marthamioni4288?si=qtd1j1BylQmggVsY" },
+  { label: "Substack", url: "https://substack.com/@marmioni?utm_source=share&utm_medium=android&r=5lxmvs" },
+  { label: "Amazon", url: "https://amazon.com/author/yourname" },
+];
+
+export const instagramLink = socialLinks.find((link) => link.label === "Instagram")!;
