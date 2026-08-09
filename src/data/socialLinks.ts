06f9cb9435c61069b94a431fdf66b14333851b07
Marmioni <maarthamaria@gmail.com>
2026-08-08 18:04:54 -0600

    Actualizar link amazon

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -2,7 +2,7 @@ export const socialLinks = [
   { label: "Instagram", url: "https://www.instagram.com/marmioni.escritora?igsh=NTVpYWg3M2dqOXZw" },
   { label: "YouTube", url: "https://youtube.com/@marthamioni4288?si=qtd1j1BylQmggVsY" },
   { label: "Substack", url: "https://substack.com/@marmioni?utm_source=share&utm_medium=android&r=5lxmvs" },
-  { label: "Amazon", url: "https://amazon.com/author/yourname" },
+  { label: "Amazon", url: "https://a.co/d/0cW7aFZW" },
 ];
 
 export const instagramLink = socialLinks.find((link) => link.label === "Instagram")!;
